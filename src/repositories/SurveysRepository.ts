939f7a5e9f6d509aee282272a527b38209cad43f
padraorezende <padraonxgalo@gmail.com>
2021-08-16 00:33:15 -0300

    Criando migration, repository e controller de pesquisa, (survey)

---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Survey } from "../models/Survey";
+
+@EntityRepository(Survey)
+export class SurveysRepository extends Repository<Survey>{
+
+}
