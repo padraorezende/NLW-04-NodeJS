7b7c414ccd62dc4adfe3d78432b2b3a78152e0f2
padraorezende <padraonxgalo@gmail.com>
2021-08-16 22:48:27 -0300

    Corrigindo problema gerado na consulta da criacao da tabela surveys

--- src/database/migrations/1628979804495-CreateSurveys.ts
+++ src/database/migrations/1628979804495-CreateSurveys.ts
@@ -21,7 +21,7 @@ export class CreateSurveys1628979804495 implements MigrationInterface {
                         type: "varchar"
                     },
                     {
-                        name: "create_at",
+                        name: "created_at",
                         type: "timestamp",
                         default: "now()",
                     }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
+import { SurveyController } from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
-import { SurveysRepository } from "./repositories/SurveysRepository";
 
 export const router = Router();
 
 const userController = new UserController();
-const surveysController = new SurveysRepository();
+const surveysController = new SurveyController();
 
 router.post("/users", userController.create)
 router.post("/surveys", surveysController.create)
