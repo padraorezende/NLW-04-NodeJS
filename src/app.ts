7adc834ef4b8226eec54046a94c1cba63c4ef9b7
padraorezende <padraonxgalo@gmail.com>
2021-08-16 23:48:27 -0300

    Inciando configuracao para iniciar testes usando o jest

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata'
+import express from 'express'
+import "./database"
+import { router } from './routes';
+
+export const app = express();
+
+app.use(express.json());
+app.use(router)
